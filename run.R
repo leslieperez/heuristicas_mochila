7b8d0ad7ac3d8bf9144ded30768e50405d81a398
leslieperez <leslie.perez@pucv.cl>
2020-10-19 12:14:09 -0300

    Comentarios y organización

--- run.R
+++ run.R
@@ -7,22 +7,22 @@ instancia <- instancia1
 print(instancia)
 
 ## Crear una solucion aleatoria
-#solucion_random(instancia)
+#solucion <- solucion_random(instancia)
 
 # Generar una solucion con la heuristica del mejor valor
-#mejor_valor(instancia)
+#solucion <- mejor_valor(instancia)
 
 # Generar una solucion con la heuristica del mejor peso
-#mejor_peso(instancia)
+#solucion <- mejor_peso(instancia)
 
 # Generar una solución con la heuristica de la mejor razon
-#mejor_razon(instancia)
+#solucion <- mejor_razon(instancia)
 
 # Leamos una instancia mas grande
 #instancia <- leer_instancia("instancias/kp-2000.txt")
 # generemos una solucion aleatoria
 #solucion <- solucion_random(instancia)
 # Apliquemos busqueda local
-#busqueda_local_bitflip(solucion, instancia)
+#solucion <- busqueda_local_bitflip(solucion, instancia)
 
 
